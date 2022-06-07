c818798338962b2892956bfc186fa109af6e3a1e
Herobrine62 <90317810+Herobrine62@users.noreply.github.com>
2022-06-07 19:14:25 +0530

    Create MAin.py

---
+++ MAin.py
@@ -0,0 +1,31 @@
+import os
+
+
+
+class Program:
+    
+    def createFile():
+        name = input("Name of the File ")
+        file = open(name,"w+")
+        print("1 = Java code")
+        print("2 = Html code")
+        choise = int(input("Input your choise number "))
+        Java = "public class Main{}"
+        Html = "<Html><Head></Head><title></title><body></body></Html>"
+        if choise == 1:
+            file.write(Java)
+        elif choise == 2:
+            file.write(Html)
+        else:
+            print("Invalid Number")
+        
+        file.close()
+    def exsyscom():
+        command = input("Any Command ")
+        os.system(command)
+    while True:
+        choise = int(input("Enter a choise number "))
+        if choise == 1:
+            createFile()
+        elif choise == 2:
+            exsyscom()
